de4b22e3f75bac344b1ca4f924e3919c340d218b
Pranav Chatur <pranavchatur@gmail.com>
2025-11-23 11:37:06 +0530

    Update styling for comfortable dark-mode view, Add Sugar to Cappuccino, Add Bundler Cafe Schematics Excalidraw (#3)
    * Add Bundler Cafe Schematics Excalidraw, Add Sugar to Cappuccino, Reorder Black and Cappuccino in the Cafe Menu, Update stylings for comfortable dark-mode view
    
    * Fix boolean coercing

--- 01-bundler-dependency-resolution/src/Cappuccino.js
+++ 01-bundler-dependency-resolution/src/Cappuccino.js
@@ -1,8 +1,10 @@
 import { Espresso } from './Espresso.js';
 import { Milk } from './Milk.js';
+import { Sugar } from './Sugar.js';
 
 export function Cappuccino() {
 	console.log('Cappucino contents:');
 	Espresso();
 	Milk();
+	Sugar();
 }
